9f05bd2b053e51baec91e8ee403379fa2416bbc6
JoseDDP <92837285+JoseDDP@users.noreply.github.com>
2021-12-12 22:29:53 -0500

    reto2

---
+++ app/src/main/java/com/example/manicuraapp3/ui/productos/ProductosFragment.java
@@ -0,0 +1,63 @@
+package com.example.manicuraapp3.ui.productos;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.manicuraapp3.FormActivity;
+import com.example.manicuraapp3.R;
+import com.example.manicuraapp3.databinding.FragmentProductosBinding;
+
+public class ProductosFragment extends Fragment {
+
+    private FragmentProductosBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentProductosBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_add:
+                Intent intent = new Intent(getContext(), FormActivity.class);
+                intent.putExtra("name","PRODUCTOS");
+                getActivity().startActivity(intent);
+                //Toast.makeText(getContext(), "Hola Productos", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
